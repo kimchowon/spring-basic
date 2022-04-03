5ea5adaa5429ab9feab78e7c788932ef2e30e3a2
김초원 <zzzcc33@naver.com>
2022-04-03 16:43:48 +0900

    - @ComponentScan - @Component

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 // @Component 애노테이션이 붙은 클래스를 찾아서 모두 자동으로 스프링빈으로 등록해준다.
-@ComponentScan(
+/*@ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)*/
+@ComponentScan(
+        basePackages = {"hello.core.member", "hello.core.order"}
 )
 public class AutoAppConfig {
 
