160f8836bc726cdc6b5196656c2d9a488b89071e
김초원 <zzzcc33@naver.com>
2022-04-05 21:40:39 +0900

    lombok

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -7,11 +7,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
-    private int discountPercent = 10;
-
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
+            int discountPercent = 10;
             return price * discountPercent / 100;
         } else {
             return 0;
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,24 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-
-    // 자동 의존관계 주입
-    // 스피링이 파라미터 타입에 맞는 빈을 가져와서 자동으로 주입해준다.
-    // ac.getBean(MemberRepository.class) 와 비슷함
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
